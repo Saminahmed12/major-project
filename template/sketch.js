1a13bb9b278ffc752f8d744a64e1cef1e9d9cc56
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-10-18 09:59:00 -0600

    added p5js template

---
+++ template/sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let carlsLeftBootyCheeksStinky = 100;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  backgdound('aqua');
+  rect(mouseX,mouseY,carlsLeftBootyCheeksStinky,100);
+}
+
