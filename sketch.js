9bb8c6e07908ddd6507551e48337ad790a9188e0
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-10-19 10:15:30 -0600

    added background options

--- sketch.js
+++ sketch.js
@@ -6,6 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let level = 1;
+let levelOneBg;
+let levelTwoBg;
+let levelThreeBg;
+
+function preload() {
+  levelOneBg = loadImage("assets/background1.jpg");
+  levelTwoBg = loadImage("assets/background2.jpg");
+  levelThreeBg = loadImage("assets/background3.jpg");
+}
 
 
 function setup() {
@@ -13,7 +23,14 @@ function setup() {
 }
 
 function draw() {
-  background("aqua");
-
+  if (level === 1) {
+    background(levelOneBg);
+  }
+  else if (level === 2) {
+    background(levelTwoBg);
+  }
+  else if (level === 3) {
+    background(levelThreeBg);
+  }
 }
 
