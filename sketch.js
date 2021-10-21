510e4a8894d5100c143133703d619bee4f3c9dbc
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-10-21 11:00:06 -0600

    flipped images for zombies

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let level = 3; 
+let level = 2; 
 // university
 let levelOneBg;
 
@@ -23,6 +23,14 @@ let zombie3;
 let zombie4;
 let zombie5;
 let zombie6;
+
+// zombies/2
+let zombie1Flipped;
+let zombie2Flipped;
+let zombie3Flipped;
+let zombie4Flipped;
+let zombie5Flipped;
+let zombie6Flipped;
 
 let zombie;
 
@@ -42,11 +50,21 @@ function preload() {
   zombie4 = loadImage("assets/zombie4.png");
   zombie5 = loadImage("assets/zombie5.png");
   zombie6 = loadImage("assets/zombie6.png");
+
+  // zombiesFlipped
+  zombie1Flipped = loadImage("assets/zombie1flipped.png");
+  zombie2Flipped = loadImage("assets/zombie2flipped.png");
+  zombie3Flipped = loadImage("assets/zombie3flipped.png");
+  zombie4Flipped = loadImage("assets/zombie4flipped.png");
+  zombie5Flipped = loadImage("assets/zombie5flipped.png");
+  zombie6Flipped = loadImage("assets/zombie6flipped.png");
+
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 
   // new zombies
   zombie = new Zombies(random(width),height-100);
@@ -78,11 +96,27 @@ class Zombies{
     this.height= 100;
     this.speed = 5;
     this.sprites = [zombie1,zombie2,zombie3,zombie4,zombie5,zombie6];
+    this.spritesFlipped = [zombie1Flipped,zombie2Flipped,zombie3Flipped,zombie4Flipped,zombie5Flipped,zombie6Flipped];
   }
 
   display(){
-    let zombieSprite = random(this.sprites);
-    image(zombieSprite ,this.x,this.y,this.width,this.height);
+    
+    console.log(millis());
+    let timer = 1000;
+    if (timer > millis()){
+      let zombieSprite = random(this.sprites);
+      let zombieSpriteFlipped = random(this.spritesFlipped);
+      if (this.x > width/2){
+        image(zombieSprite ,this.x,this.y,this.width,this.height);
+        timer = timer+millis();
+      }
+      else if (this.x < width/2){
+        image(zombieSpriteFlipped ,this.x,this.y,this.width,this.height);
+        timer = timer+millis();
+      }
+    }
+    
+    
     
   }
 
